e2371d8f224ca5618c98311896673d0ebd896969
Adhithyakrishna <adhithyakrishna@skava.com>
2017-05-29 23:16:49 +0530

    js to calculate width

--- public/js/portfolio.js
+++ public/js/portfolio.js
@@ -1,26 +1,34 @@
 $(document).ready(function() {
 
-	  var isMobile = window.matchMedia("only screen and (max-width: 768px)");
+	var isMobile = window.matchMedia("only screen and (max-width: 768px)");
 
-	   if (isMobile.matches) {
-	   		$('body').addClass('ismobile');
-	   }
-	
+	if (isMobile.matches) {
+		$('body').addClass('ismobile');
+	}
+
+	var calcWidth = function() {
 		headerWidth = $("#educationHeader").innerWidth() - 76;
 		$(".contentEducationHolder").innerWidth(headerWidth);
-	
-	 setFlag = "true";
-	 urlHolder = {
-		"sprite" : "//spritemapper.herokuapp.com",
+	}
+
+	calcWidth();
+
+	$(window).resize(function() {
+		calcWidth();
+	});
+
+	setFlag = "true";
+	urlHolder = {
+		"sprite": "//spritemapper.herokuapp.com",
 		"todo": "//mytasklistapp.herokuapp.com/task",
-		"photo" : "" ,
+		"photo": "",
 		"teresa": "//veraciousverves.blogspot.in/2013/12/mother-teresa.html",
-		"civilWar" : "//veraciousverves.blogspot.in/2013/12/civil-war.html",
-		"friendship" : "//veraciousverves.blogspot.in/2013/12/friendship.html",
-		"savegirlChild" : "//veraciousverves.blogspot.in/2013/12/female-infanticide.html",
-		"dearCupid" :  "//veraciousverves.blogspot.in/2016/12/dear-cupid.html",
-		"celestial" : "//veraciousverves.blogspot.in/2017/01/celestial-love.html",
-		"quintessence" : "//veraciousverves.blogspot.in/2015/04/quintessence-of-life.html"
+		"civilWar": "//veraciousverves.blogspot.in/2013/12/civil-war.html",
+		"friendship": "//veraciousverves.blogspot.in/2013/12/friendship.html",
+		"savegirlChild": "//veraciousverves.blogspot.in/2013/12/female-infanticide.html",
+		"dearCupid": "//veraciousverves.blogspot.in/2016/12/dear-cupid.html",
+		"celestial": "//veraciousverves.blogspot.in/2017/01/celestial-love.html",
+		"quintessence": "//veraciousverves.blogspot.in/2015/04/quintessence-of-life.html"
 	}
 
 	function setData() {
@@ -32,118 +40,115 @@ $(document).ready(function() {
 	setData();
 
 	$('.sliderHolder').slick({
-		  // centerMode: true,
-    	  // centerPadding: '40px',
-      	  pauseOnHover: true,
-      	  // focusOnSelect :true,
-      	  infinite: true,
-      	  slidesToShow: 3,
- 		  slidesToScroll: 2,
-      	  autoplay: true,
-      	  autoplaySpeed: 3000,
-      	  prevArrow:"<div class='previousArrow'><div class='arrowholderLeft'></div></div>",
-  	      nextArrow:"<div class='rightArrow'><div class='arrowholderRight'></div></div>",
-	      responsive: [
-	      {
-            breakpoint: 769,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1,
-            }
-          },{
-          	breakpoint: 640,
-          	settings: {
-          	  slidesToShow: 1,
-          	  slidesToScroll: 1,
-	      	  autoplaySpeed: 6000,
-          	}
-          }]
+		// centerMode: true,
+		// centerPadding: '40px',
+		pauseOnHover: true,
+		// focusOnSelect :true,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 2,
+		autoplay: true,
+		autoplaySpeed: 3000,
+		prevArrow: "<div class='previousArrow'><div class='arrowholderLeft'></div></div>",
+		nextArrow: "<div class='rightArrow'><div class='arrowholderRight'></div></div>",
+		responsive: [{
+			breakpoint: 769,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 1,
+			}
+		}, {
+			breakpoint: 640,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				autoplaySpeed: 6000,
+			}
+		}]
 
-      	  // prevArrow : "previous",
-      	  // nextArrow : "next"
-      });
+		// prevArrow : "previous",
+		// nextArrow : "next"
+	});
 
 	$.each($(".projectDescription"), function(index, val) {
 		var setheight = 0
-			if($(this).height() > setheight)
-			{
-				setheight = $(this).height();
-			}
-			if(setheight) {
-				$(".buildDescription").height(setheight + 52);
-			}
+		if ($(this).height() > setheight) {
+			setheight = $(this).height();
+		}
+		if (setheight) {
+			$(".buildDescription").height(setheight + 52);
+		}
 	});
 
-	function animateIt()
-	{
-	    $('.skillbar').each(function(){
-	        $(this).find('.skillbar-bar').animate({
-	            width:$(this).attr('data-percent')
-	        },2000);
-	    });
-	    $(".skillContainer").addClass('animated');
+	function animateIt() {
+		$('.skillbar').each(function() {
+			$(this).find('.skillbar-bar').animate({
+				width: $(this).attr('data-percent')
+			}, 2000);
+		});
+		$(".skillContainer").addClass('animated');
 	}
 
 	function isScrolledIntoView(elem) {
 
-	    var docViewTop = $(window).scrollTop();
-	    var docViewBottom = docViewTop + $(window).height();
+		var docViewTop = $(window).scrollTop();
+		var docViewBottom = docViewTop + $(window).height();
 
-	    var elemTop = $(elem).offset().top;
-	    var elemBottom = elemTop + $(elem).height();
+		var elemTop = $(elem).offset().top;
+		var elemBottom = elemTop + $(elem).height();
 
-	    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+		return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 
 	}
 
 	function allInView() {
 
-	    if(isScrolledIntoView($(".animateit")))
-	    {
-	        animateIt();
-	    }
+		if (isScrolledIntoView($(".animateit"))) {
+			animateIt();
+		}
 	}
 
 	var scrollFunction = function() {
 		$(window).scroll(function(event) {
-			if(!($(".skillContainer").hasClass('animated')) && setFlag) {
-				setFlag="false";
-			    allInView();
+			if (!($(".skillContainer").hasClass('animated')) && setFlag) {
+				setFlag = "false";
+				allInView();
 			}
 		});
 	}
 
-	if(!$('body').hasClass('ismobile'))
-	{
+	if (!$('body').hasClass('ismobile')) {
 		scrollFunction();
 	}
 
-	$( ".project , .writings" ).hover(
-	  function() {
-	    $( this ).addClass( "animator" );
-	  }, function() {
-	    $( this ).removeClass( "animator" );
-	  }
+	$(".project , .writings").hover(
+		function() {
+			$(this).addClass("animator");
+		},
+		function() {
+			$(this).removeClass("animator");
+		}
 	);
-	$( ".writings" ).hover(
-	  function() {
-	  	$(".sliderHolder").addClass("pauseIt");
-	    $( this ).addClass( "animator" );
-	  }, function() {
-	    $( ".sliderHolder" ).removeClass( "pauseIt" );
-	    $( this ).removeClass( "animator" );
-	  }
+	$(".writings").hover(
+		function() {
+			$(".sliderHolder").addClass("pauseIt");
+			$(this).addClass("animator");
+		},
+		function() {
+			$(".sliderHolder").removeClass("pauseIt");
+			$(this).removeClass("animator");
+		}
 	);
 
 	$(".socialImg").unbind('click').bind('click', function(event) {
 		var navUrl = $(this).siblings('.urlHolder').find('.uri').trigger('click')[0];
 		$(navUrl)[0].click();
-	});	
+	});
 
 	$(".writings, .project").unbind('click').bind('click', function(event) {
 		var selectedContent = $(this).attr('id');
 		var navUrl = urlHolder[selectedContent];
 		// console.log(navUrl);
-		window.open(navUrl, '_blank'); 
+		window.open(navUrl, '_blank');
 	});
 });
